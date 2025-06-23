06068ee188e7aec7e5d9817eba775a5491454c35
Matheus Leao <matsouzaleao@gmail.com>
2025-06-23 14:53:54 -0300

    Make initial changes on the layoult

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import {
-  FaFacebookF,
-  FaTwitter,
   FaLinkedinIn,
-  FaReact,
   FaGithub,
 } from "react-icons/fa";
-import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+import { SiSelenium, SiCypress, SiAzuredevops, SiPostman } from "react-icons/si";
 
 const LeftBanner = () => {
   const [text] = useTypewriter({
-    words: ["Professional Coder.", "Full Stack Developer.", "UI Designer."],
+    words: ["Quality Assurance Engineer.", "Quality Automation Engineer.", "Software Developer in Test."],
     loop: true,
     typeSpeed: 20,
     deleteSpeed: 10,
@@ -22,7 +19,7 @@ const LeftBanner = () => {
       <div className="flex flex-col gap-5">
         <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
         <h1 className="text-6xl font-bold text-white">
-          Hi, I'm <span className="text-designColor capitalize">John doe</span>
+          Hi, I'm <span className="text-designColor capitalize">Matheus Leão</span>
         </h1>
         <h2 className="text-4xl font-bold text-white">
           a <span>{text}</span>
@@ -33,9 +30,10 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide">
-          I use animation as a third dimension by which to simplify experiences
-          and kuiding thro each and every interaction. I'm not adding motion
-          just to spruce things up, but doing it in ways that.
+          With over 9 years of professional experience, I have dedicated the last 8 years to Software 
+          Quality Testing and Automation. I specialize in End-to-End (E2E), Mobile, and API Testing, 
+          utilizing Cypress, TypeScript, Selenium, C#, and various automation frameworks to ensure 
+          high-quality software delivery
         </p>
       </div>
       <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
@@ -44,22 +42,12 @@ const LeftBanner = () => {
             Find me in
           </h2>
           <div className="flex gap-4">
-            <a href="https://github.com/noorjsdivs/portfolioone" target="blank">
+            <a href="https://github.com/matheus-leao" target="blank">
               <span className="bannerIcon">
                 <FaGithub />
               </span>
             </a>
-            <a href="https://github.com/noorjsdivs/portfolioone" target="blank">
-              <span className="bannerIcon">
-                <FaFacebookF />
-              </span>
-            </a>
-            <a href="https://github.com/noorjsdivs/portfolioone" target="blank">
-              <span className="bannerIcon">
-                <FaTwitter />
-              </span>
-            </a>
-            <a href="https://github.com/noorjsdivs/portfolioone" target="blank">
+            <a href="https://www.linkedin.com/in/matheus-leao-br/" target="blank">
               <span className="bannerIcon">
                 <FaLinkedinIn />
               </span>
@@ -72,16 +60,16 @@ const LeftBanner = () => {
           </h2>
           <div className="flex gap-4">
             <span className="bannerIcon">
-              <FaReact />
+              <SiCypress />
             </span>
             <span className="bannerIcon">
-              <SiNextdotjs />
+              <SiSelenium />
             </span>
             <span className="bannerIcon">
-              <SiTailwindcss />
+              <SiPostman />
             </span>
             <span className="bannerIcon">
-              <SiFigma />
+              <SiAzuredevops />
             </span>
           </div>
         </div>
