8e7bdb84a2b12b344c6b1bcc09b0994b6eb67076
Matheus Leao <matsouzaleao@gmail.com>
2025-12-26 18:41:14 -0300

    Update feature description

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -15,37 +15,33 @@ const Features = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         <Card
           title="Test Strategy"
-          des="Define and implement Test Strategy, including Test Plan design and Optimization. "
-         
+          des="Define and implement Test Strategy, including Test Plan design and Optimization."
         />
         <Card
           title="Automation Strategy"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
+          des="Define and implement Automation Strategy on all phases of the 
+          Software Development Life Cycle."
           icon={<AiFillAppstore />}
         />
         <Card
           title="API & e2e Testing"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
+          des="Create and define automation projects on API and e2e testing 
+          with many tools and languages."
           icon={<SiProgress />}
         />
         <Card
           title="Pipeline Design and Optimization"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
+          des="Design and optimize pipelines using YAML and Azure Devops. "
           icon={<FaMobile />}
         />
         <Card
           title="Synthetic and Monitoring"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
+          des="Create synthetic tests to improve monitoring with alerts and thresholds."
           icon={<SiAntdesign />}
         />
         <Card
           title="Mentorship"
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque soluta
-          hic consequuntur eum repellendus ad."
+          des="Mentored QA team members in e2e testing, system design, and quality best practices."
           icon={<FaGlobe />}
         />
       </div>
